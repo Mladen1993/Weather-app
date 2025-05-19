213e63708cfb59732a16d1e56eb01440dcee9a3b
Mladen kitic <mladenkitic1993@gmail.com>
2025-05-19 21:20:42 +0200

    start project

--- src/script.js
+++ src/script.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const apiKey = "e0bed337d7c742e29de190826251905";
+
 const response = await axios.get("https://dummyjson.com/test");
 
 console.log(response.data.status);
