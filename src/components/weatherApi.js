63b6bbc1ed2e93c264b562a476a5c6e4e05538bd
Mladen kitic <mladenkitic1993@gmail.com>
2025-05-25 16:25:13 +0200

    .

--- src/components/weatherApi.js
+++ src/components/weatherApi.js
@@ -1,10 +1,9 @@
-const apiKey = "e0bed337d7c742e29de190826251905";
 import axios from "axios";
 
 export async function getCurrentWeatherForLocation(location) {
-  return await axios.get("https://api.weatherapi.com/v1/current.json", {
+  return await axios.get(process.env.API_URL + "/v1/current.json", {
     params: {
-      key: apiKey,
+      key: process.env.API_KEY,
       q: location,
       aqi: "no",
     },
@@ -12,9 +11,9 @@ export async function getCurrentWeatherForLocation(location) {
 }
 
 export async function getforecastForSeveralDays(location, days) {
-  return await axios.get("https://api.weatherapi.com/v1/forecast.json", {
+  return await axios.get(process.env.API_URL + "/v1/forecast.json", {
     params: {
-      key: apiKey,
+      key: process.env.API_KEY,
       q: location,
       days: days,
       aqi: "no",
@@ -22,18 +21,3 @@ export async function getforecastForSeveralDays(location, days) {
     },
   });
 }
-
-// const datas = response.data.forecast.forecastday;
-// let forecastDiv = document.querySelector(".forecast");
-// forecastDiv.innerHTML = "";
-
-// for (let data of datas) {
-//   let forecastEl = document.createElement("div");
-
-//   forecastEl.innerHTML = ` <p>Date: ${data.date} </p>
-//                           <p>Maxtepm: ${data.day.maxtemp_c} </p>
-//                           <p>Mintepm: ${data.day.mintemp_c} </p>`;
-//   forecastDiv.append(forecastEl);
-
-//   console.log(data);
-// }
